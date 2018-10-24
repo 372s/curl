001bac9fab62eff45579fcbe293d65d0fb3271a5
wangqiang <wq455133@163.com>
2018-10-24 10:38:48 +0800

    增加 https请求
    增加 https请求

--- src/Curl.php
+++ src/Curl.php
@@ -49,6 +49,7 @@ class Curl
         curl_setopt($curl, CURLOPT_URL, $api);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
         curl_setopt($curl, CURLOPT_HEADER, 0);
         curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 
